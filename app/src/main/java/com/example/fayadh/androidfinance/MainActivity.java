dbfe5248dec5a29d86b7497228aeaad3c54c7921
Fayadh A <fayadh56@gmail.com>
2016-10-16 03:48:03 -0400

    Commited

--- app/src/main/java/com/example/fayadh/androidfinance/MainActivity.java
+++ app/src/main/java/com/example/fayadh/androidfinance/MainActivity.java
@@ -12,10 +12,7 @@ import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Double totalBalance = 0.0;
-    String FILENAME = "data_file";
-    String stringOf = "";
-    String stringSaved = "";
+    float totalBalance = 0.0f;
 
 
     public MainActivity() throws FileNotFoundException {
@@ -25,40 +22,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        loadData();
 
     }
 
     public void getBalance(View v) {
 
-        Double currentBalance = 0.0;
+        float currentBalance = 0.0f;
 
         try {
             EditText balance = (EditText) findViewById(R.id.edit);
+            String stringOf;
             stringOf = balance.getText().toString();
-            currentBalance = Double.parseDouble(stringOf);
+            currentBalance = Float.parseFloat(stringOf);
         } catch (NumberFormatException e) {
         }
 
         totalBalance += currentBalance;
+        setBalance();
 
-        TextView myTextView = (TextView) findViewById(R.id.textBalance);
-        myTextView.setText("$ " + totalBalance);
+        saveData();
     }
 
     private void saveData() {
         SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString("currency", stringOf);
+        editor.putFloat("balance", totalBalance);
+        editor.commit();
     }
 
     private void loadData() {
         SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        stringSaved = sp.getString("currency", stringOf);
+        totalBalance = sp.getFloat("balance", totalBalance);
 
-        TextView myTextView = (TextView) findViewById(R.id.textBalance);
-        myTextView.setText("$ " + stringOf);
+        setBalance();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadData();
+    }
+
+    public void resetBalance(View v) {
+        totalBalance = 0.0f;
+        saveData();
+        setBalance();
+    }
+
+    public void setBalance() {
+        TextView myTextView = (TextView) findViewById(R.id.textBalance);
+        myTextView.setText("$ " + totalBalance);
+    }
+
+
 
 
 }
