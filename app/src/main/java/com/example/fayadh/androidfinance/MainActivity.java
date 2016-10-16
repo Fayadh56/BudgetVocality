24d3ac2b2c73f40fd789e17b253432728a884491
Fayadh A <fayadh56@gmail.com>
2016-10-15 21:36:34 -0400

    Commited

--- app/src/main/java/com/example/fayadh/androidfinance/MainActivity.java
+++ app/src/main/java/com/example/fayadh/androidfinance/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.fayadh.androidfinance;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getBalance(View v) {
+
+        EditText balance = (EditText) findViewById(R.id.edit);
+        String stringOf = balance.getText().toString();
+
+        TextView myTextView = (TextView) findViewById(R.id.textBalance);
+        myTextView.setText("$" + stringOf);
     }
 }
